383b1d5aa565113cc7d4f2b9da3d64438c4f9991
YOUR NAME <you@example.com>
2015-02-27 12:13:15 -0800

    Initial Commit

---
+++ TestShop/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  TestShop
+//
+//  Created by Vincent Lee on 2/27/15.
+//  Copyright (c) 2015 Analytics Pros. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ TestShop/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  TestShop
+//
+//  Created by Vincent Lee on 2/27/15.
+//  Copyright (c) 2015 Analytics Pros. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
